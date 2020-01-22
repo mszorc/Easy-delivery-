5988d23ca79956864cfc60ba9e25cee48aedd510
Paweł Bąk <pawel.bak98@gmail.com>
2020-01-22 11:21:58 +0100

    more visual changes

---
+++ app/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer class="page-footer font-small">
+        <div class="container">© 2020 Copyright: Paweł Bąk & Michał Szorc</div>
+      </footer>
+    );
+  }
+}
